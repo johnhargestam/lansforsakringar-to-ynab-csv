40eefe615c33fe8404d67c2a41f27ce799bb7bf2
= <john.hargestam@gmail.com>
2021-10-17 11:11:04 +0200

    add chunk function

--- src/csv.test.ts
+++ src/csv.test.ts
@@ -1,7 +1,21 @@
-import { split } from "./csv"
+import { chunk, split } from "./csv"
+
+describe('chunk', () => {
+  it('divides a string into an array split on empty lines', () => {
+    expect(chunk('a\nb\n\nc\nd')).toStrictEqual(['a\nb', 'c\nd']);
+  });
+
+  it('splits on new lines with carriage return', () => {
+    expect(chunk('a\r\n\r\nb')).toStrictEqual(['a', 'b']);
+  });
+
+  it('splits on multiple lines containing whitespace', () => {
+    expect(chunk('a\r\n\t\r\n \nb')).toStrictEqual(['a', 'b']);
+  });
+})
 
 describe('split', () => {
-  it('splits a string into a list of lines', () => {
-    expect(split('1\n2\n3')).toHaveLength(3);
-  })
-})
+  it('divides a string into an array of lines', () => {
+    expect(split('a\nb\nc')).toStrictEqual(['a', 'b', 'c']);
+  });
+});
