f9aaf8be5445846556f8f50cd0420479732b4f08
= <john.hargestam@gmail.com>
2021-10-17 13:03:53 +0200

    add amount format function

--- src/format.test.ts
+++ src/format.test.ts
@@ -1,7 +1,13 @@
-import { date } from './format';
+import { amount, date } from './format';
 
 describe('date', () => {
   it('formats to us date notation', () => {
     expect(date('2021-12-01')).toStrictEqual('12/01/21');
+  });
+});
+
+describe('amount', () => {
+  it('formats to us amount notation', () => {
+    expect(amount('-1 215,95')).toStrictEqual('-1215.95');
   });
 });
--- src/format.ts
+++ src/format.ts
@@ -3,3 +3,6 @@ export const date = (date: string) =>
     /\d{2}(\d{2})-(\d{2})-(\d{2})/,
     (_, year, month, day) => `${month}/${day}/${year}`
   );
+
+export const amount = (amount: string) =>
+  amount.replace(/\s/g, '').replace(/,/, '.');
